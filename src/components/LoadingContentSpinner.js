e1dd4a2ce447b0e0934f76f27385dcf4870cbe6b
Aamir Hussain <aamir222686@yahoo.com>
2023-02-18 13:44:57 +0530

    git issue

--- src/components/LoadingContentSpinner.js
+++ src/components/LoadingContentSpinner.js
@@ -4,6 +4,7 @@ import Grid from '@mui/material/Grid';
 
 function LoadingContentSpinner() {
     return (
+        // Grid layout
         <Grid container style={{ display: "flex", width: "100%", height: "75vh", justifyContent: "center", alignItems: "center" }}>
             <CssVarsProvider>
                 <CircularProgress variant="solid" size="md" />

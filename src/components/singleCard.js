9f7cd46acf5c2942b943c5d12b4706a2c1b65bd9
Aamir Hussain <aamir222686@yahoo.com>
2023-03-16 21:58:33 +0530

    simplified some ui elements

--- src/components/singleCard.js
+++ src/components/singleCard.js
@@ -32,19 +32,19 @@ export default function SingleCard({ post, clicked }) {
 
   const categoryChip = (category) => {
     if (category == "startup") {
-      return <a style={{justifySelf: "flex-end", alignSelf: "flex-end"}} href={`/${category}`}><Chip sx={{ textTransform: "capitalize", cursor: "pointer", justifySelf: "flex-end", alignSelf: "flex-end", margin: "0px", fontSize: "0.7rem", fontWeight: "bold", backgroundColor: "#5200ff", color: "white", borderRadius: 0, position: "relative", top: "-30px", boxShadow: "2px 2px 2px #890084"}} label={`${category}`} variant="filled" size="small" /></a>
+      return <a style={{justifySelf: "flex-end", alignSelf: "flex-end"}} href={`/${category}`}><Chip sx={{ textTransform: "capitalize", cursor: "pointer", justifySelf: "flex-end", alignSelf: "flex-end", margin: "0px", fontSize: "0.7rem", fontWeight: "bold", backgroundColor: "#5200ff", color: "white", borderRadius: 0, position: "relative", top: "-30px"}} label={`${category}`} variant="filled" size="small" /></a>
     }
     if (category == "technology") {
-      return <a style={{justifySelf: "flex-end", alignSelf: "flex-end"}} href={`/${category}`}><Chip sx={{ textTransform: "capitalize", cursor: "pointer", justifySelf: "flex-end", alignSelf: "flex-end", margin: "0px", fontSize: "0.7rem", fontWeight: "bold", backgroundColor: "#5200ff", color: "white", borderRadius: 0, position: "relative", top: "-30px", boxShadow: "2px 2px 2px #890084"}} label={`${category}`} variant="filled" size="small" /></a>
+      return <a style={{justifySelf: "flex-end", alignSelf: "flex-end"}} href={`/${category}`}><Chip sx={{ textTransform: "capitalize", cursor: "pointer", justifySelf: "flex-end", alignSelf: "flex-end", margin: "0px", fontSize: "0.7rem", fontWeight: "bold", backgroundColor: "#5200ff", color: "white", borderRadius: 0, position: "relative", top: "-30px"}} label={`${category}`} variant="filled" size="small" /></a>
     }
     if (category == "ai") {
-      return <a style={{justifySelf: "flex-end", alignSelf: "flex-end"}} href={`/${category}`}><Chip sx={{ textTransform: "capitalize", cursor: "pointer", justifySelf: "flex-end", alignSelf: "flex-end", margin: "0px", fontSize: "0.7rem", fontWeight: "bold", backgroundColor: "#5200ff", color: "white", borderRadius: 0, position: "relative", top: "-30px", boxShadow: "2px 2px 2px #890084"}} label={`${category}`} variant="filled" size="small" /></a>
+      return <a style={{justifySelf: "flex-end", alignSelf: "flex-end"}} href={`/${category}`}><Chip sx={{ textTransform: "capitalize", cursor: "pointer", justifySelf: "flex-end", alignSelf: "flex-end", margin: "0px", fontSize: "0.7rem", fontWeight: "bold", backgroundColor: "#5200ff", color: "white", borderRadius: 0, position: "relative", top: "-30px"}} label={`${category}`} variant="filled" size="small" /></a>
     }
     if (category == "science") {
-      return <a style={{justifySelf: "flex-end", alignSelf: "flex-end"}} href={`/${category}`}><Chip sx={{ textTransform: "capitalize", cursor: "pointer", justifySelf: "flex-end", alignSelf: "flex-end", margin: "0px", fontSize: "0.7rem", fontWeight: "bold", backgroundColor: "#5200ff", color: "white", borderRadius: 0, position: "relative", top: "-30px", boxShadow: "2px 2px 2px #890084"}} label={`${category}`} variant="filled" size="small" /></a>
+      return <a style={{justifySelf: "flex-end", alignSelf: "flex-end"}} href={`/${category}`}><Chip sx={{ textTransform: "capitalize", cursor: "pointer", justifySelf: "flex-end", alignSelf: "flex-end", margin: "0px", fontSize: "0.7rem", fontWeight: "bold", backgroundColor: "#5200ff", color: "white", borderRadius: 0, position: "relative", top: "-30px"}} label={`${category}`} variant="filled" size="small" /></a>
     }
     if (category == "entertainment") {
-      return <a style={{justifySelf: "flex-end", alignSelf: "flex-end"}} href={`/${category}`}><Chip sx={{ textTransform: "capitalize", cursor: "pointer", justifySelf: "flex-end", alignSelf: "flex-end", margin: "0px", fontSize: "0.7rem", fontWeight: "bold", backgroundColor: "#5200ff", color: "white", borderRadius: 0, position: "relative", top: "-30px", boxShadow: "2px 2px 2px #890084"}} label={`${category}`} variant="filled" size="small" /></a>
+      return <a style={{justifySelf: "flex-end", alignSelf: "flex-end"}} href={`/${category}`}><Chip sx={{ textTransform: "capitalize", cursor: "pointer", justifySelf: "flex-end", alignSelf: "flex-end", margin: "0px", fontSize: "0.7rem", fontWeight: "bold", backgroundColor: "#5200ff", color: "white", borderRadius: 0, position: "relative", top: "-30px"}} label={`${category}`} variant="filled" size="small" /></a>
     }
   }
 

d965c208e082f775cd08de765cdbd33148327949
Aamir Hussain <aamir222686@yahoo.com>
2025-06-15 21:58:42 +0530

    reinitiated

--- src/seo/config.js
+++
@@ -1,7 +0,0 @@
-exports.seoConfig = {
-    host: "https://thehonestmachine.com",
-    title: "The Honest Machine",
-    description: "The Honest Machine is a blog website that focuses on providing honest and insightful articles about trending technologies. From the latest gadgets and software to cutting-edge advancements in the tech industry, The Honest Machine aims to keep readers informed and up-to-date. The website is a go-to source for technology enthusiasts and professionals alike who are looking for reliable and informative content. With a team of knowledgeable and experienced writers, The Honest Machine provides a fresh perspective on the ever-changing world of technology.",
-    mainBlogLogo: "https://firebasestorage.googleapis.com/v0/b/thm-image-save.appspot.com/o/thm-400.png?alt=media&token=56f1d130-e8b1-4895-885a-8bd6515300cb",
-    keywords: "THM, The Honest Machine, Automated Articles, Articles By GPT, GPT, GPT3, Chat GPT, Technology news, Gadget reviews, Software updates, Tech trends, Artificial intelligence, Internet of Things (IoT), Cybersecurity, Cloud computing, Virtual reality, Augmented reality, Machine learning, Data science, Mobile apps, Social media, Wearable technology, Blockchain, Tech startups, Digital marketing, E-commerce, Gaming"
-}
